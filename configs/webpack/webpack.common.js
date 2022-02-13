405f2006c8be108272cab919f1c9b3673f554e99
Romuelson <Romanch030@mail.ru>
2022-02-13 10:38:17 +0500

    Добавит загрузку переменных среды

--- configs/webpack/webpack.common.js
+++ configs/webpack/webpack.common.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+	prev[`process.env.${next}`] = JSON.stringify(env[next]);
+	return prev;
+}, {});
 
 module.exports = {
     entry: {
         index: './src/index.js'
     },
+    plugins: [
+		new webpack.DefinePlugin(envKeys)
+	],
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
